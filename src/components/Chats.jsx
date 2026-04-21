24cfab2be551a94f3cd5c9e4810a1c4dfce2076b
Diego Alejandro Delgado Gontes <diegodelgadog1@gmail.com>
2026-04-20 20:07:24 -0600

    ssdsdg

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,7 @@
 
 
 import React, { useEffect, useState } from 'react'
-import { socket } from '../../socket'
+import { socket } from '../socket'
 
 const Chats = () => {
 
--- src/components/ManageConnection.jsx
+++ src/components/ManageConnection.jsx
@@ -1,7 +1,7 @@
 
 
 import React from 'react';
-import { socket } from '../../socket'
+import { socket } from '../socket'
 
 const ManageConnection = () => {
     
